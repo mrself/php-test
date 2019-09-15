e10314f4f80085b576d752738f0000fd44b006ac
Lightman <yaruk.my@gmail.com>
2019-09-15 13:14:39 +0300

    refactor: move stuff from ChecklistController to ChecklistResponse

--- src/Controller/Api/ChecklistController.php
+++ src/Controller/Api/ChecklistController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller\Api;
 
-use App\Services\ContentChecker;
-use App\ValidationResponse\ChecklistValidationResponse;
-use App\ValidationResponse\InvalidChecklistRequestException;
+use App\Response\ChecklistResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,49 +12,19 @@ class ChecklistController extends AbstractController
     /**
      * @Route("/checklist", name="checklist", methods={"POST"})
      * @param Request $request
-     * @param ChecklistValidationResponse $validationResponse
-     * @param ContentChecker $contentChecker
+     * @param ChecklistResponse $response
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function index(
         Request $request,
-        ChecklistValidationResponse $validationResponse,
-        ContentChecker $contentChecker
+        ChecklistResponse $response
     ) {
         $content = $request->request->get('content', '');
-        $validationResponse->setFields([
-            'content' => $content
-        ]);
-
-        try {
-            $validationResponse->validate();
-        } catch (InvalidChecklistRequestException $e) {
-            return $this->json([
-                'success' => false,
-                'errors' => $e->getErrors()
-            ]);
-        }
-
-        $checklistParams = $this->getParameter('checklist');
-        $contentChecker->setOptions([
-            'minWordsCount' => $checklistParams['min_words_count'],
-            'keywords' => $checklistParams['keywords']
-        ]);
-
-        $contentChecker->init($content);
-        if (!$contentChecker->check()) {
-            return $this->json([
-                'success' => false,
-                'reason' => 'Lack of words in the content',
-                'words_count' => $contentChecker->getWordsCount(),
-                'minimal_words_count' => $checklistParams['min_words_count']
-            ]);
-        }
-
-        return $this->json([
+        $response->setOptions([
             'content' => $content,
-            'keywords used' => $contentChecker->getKeywordsUsedCount(),
-            'average keywords density' => $contentChecker->getKeywordsDensity()
+            'params' => $this->getParameter('checklist')
+
         ]);
+        return $this->json($response->makeResponse());
     }
 }
---
+++ src/Response/ChecklistResponse.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+namespace App\Response;
+
+use App\Services\ContentChecker;
+use App\ValidationResponse\ChecklistValidationResponse;
+use App\ValidationResponse\InvalidChecklistRequestException;
+
+class ChecklistResponse
+{
+    /**
+     * @var array
+     */
+    private $options;
+
+    /**
+     * @var ChecklistValidationResponse
+     */
+    private $validationResponse;
+
+    /**
+     * @var ContentChecker
+     */
+    private $contentChecker;
+
+    public function __construct(
+        ChecklistValidationResponse $validationResponse,
+        ContentChecker $contentChecker
+    ) {
+        $this->validationResponse = $validationResponse;
+        $this->contentChecker = $contentChecker;
+    }
+
+    public function setOptions(array $options)
+    {
+        $this->options = $options;
+    }
+
+    public function makeResponse()
+    {
+        $response = $this->getValidationResponse();
+        if ($response) {
+            return $response;
+        }
+
+        $this->initContentChecker();
+
+        if (!$this->contentChecker->check()) {
+            return $this->makeLackOfWordsResponse();
+        }
+
+        return $this->makeSuccessResponse();
+    }
+
+    private function makeSuccessResponse()
+    {
+        return [
+            'content' => $this->options['content'],
+            'keywords used' => $this->contentChecker->getKeywordsUsedCount(),
+            'average keywords density' => $this->contentChecker->getKeywordsDensity()
+        ];
+    }
+
+    private function makeLackOfWordsResponse()
+    {
+        return [
+            'success' => false,
+            'reason' => 'Lack of words in the content',
+            'words_count' => $this->contentChecker->getWordsCount(),
+            'minimal_words_count' => $this->options['params']['min_words_count']
+        ];
+    }
+
+    private function getValidationResponse(): ?array
+    {
+        $this->validationResponse->setFields([
+            'content' => $this->options['content']
+        ]);
+
+        try {
+            $this->validationResponse->validate();
+        } catch (InvalidChecklistRequestException $e) {
+            return [
+                'success' => false,
+                'errors' => $e->getErrors()
+            ];
+        }
+
+        return null;
+    }
+
+    private function initContentChecker()
+    {
+        $this->contentChecker->setOptions([
+            'minWordsCount' => $this->options['params']['min_words_count'],
+            'keywords' => $this->options['params']['keywords']
+        ]);
+
+        $this->contentChecker->init($this->options['content']);
+    }
+}
